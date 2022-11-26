3a16a9df2ef361dc098afac58b31b7e737394380
lenkoras <ivansakharuk@gmail.com>
2022-11-27 02:55:12 +0300

    Added a test project demonstrate creating services with dependencies.

---
+++ Test/App.cs
@@ -0,0 +1,13 @@
+﻿using Services;
+
+IServiceCollection collection = new ServiceCollection();
+
+collection
+    .AddValue<ICollection<User>>(new List<User>() { new("Eva"), new("VaiNa"), new("Armstrong"), new("Dee") })
+    .AddTransient<IWriter<User>, ConsoleUserWriter>()
+    .AddTransient<ICollectionWriter, CollectionWriter<User>>()
+    .Initialize();
+
+ICollectionWriter? room = collection.GetService<ICollectionWriter>();
+
+room?.WriteAll();
---
+++ Test/ConsoleUserWriter.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+
+public class ConsoleUserWriter : IWriter<User>
+{
+    private StringBuilder builder;
+    private int index;
+
+    public ConsoleUserWriter()
+    {
+        builder = new();
+        index = 1;
+    }
+
+    public void Append(User user)
+    {
+        OnAppend();
+        builder
+            .AppendLine()
+            .Append(index++)
+            .Append(". Name: ")
+            .Append(user.Name);
+    }
+
+    public void Write()
+    {
+        OnWrite();
+        Console.WriteLine(builder);
+    }
+
+    protected virtual void OnAppend()
+    {
+        if (builder.Length < 1)
+        {
+            builder.Append("Users: ");
+        }
+    }
+
+    protected virtual void OnWrite()
+    {
+        if (builder.Length < 1)
+        {
+            builder.Append("Users are missing");
+        }
+    }
+}
